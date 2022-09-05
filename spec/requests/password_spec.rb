734de6aeb963542b8fe2942469f7d516816e4d5a
Khuram Dogar <khuramdogar73@gmail.com>
2022-09-05 08:50:56 +0500

    write User auth specs

---
+++ spec/requests/password_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe "Passwords", type: :request do
+  describe "Forgot and Reset Passwords" do
+		
+		before(:each) do
+			@user1 = {email: "jane@doe.com", password: "pw1234", password_confirmation: 'pw1234', first_name: "Anything", last_name: "Lorem ipsum"}
+			@user2 = { user: {email: "john@doe.com", password: "password", password_confirmation: 'password', first_name: "John", last_name: "Doe"}}
+			user = User.create(@user1)
+			@token = user.send_password_reset
+		end
+		
+		it "Forgot Password Works" do
+			post password_resets_path, params: { user: {email: @user1[:email] }}
+			expect(flash[:success]).to eq('E-mail sent with password reset instructions')
+			expect(ActionMailer::Base.deliveries.count).to eq(2)
+			expect(ActionMailer::Base.deliveries.first.to.first).to eq(@user1[:email])
+			expect(ActionMailer::Base.deliveries.first.subject).to eq('Reset password instructions')
+			expect(ActionMailer::Base.deliveries.first.body).to include('To reset your password, click the URL below:')
+			expect(response).to redirect_to(login_path)
+		end
+		
+		it "Reset Password View works with valid reset_password_token" do
+			get edit_password_reset_path(@token)
+			expect(response.status).to eq(200)
+			expect(response.body).to include('Reset Password')
+		end
+		
+		it "Reset Password View does not works with Invalid reset_password_token" do
+			get edit_password_reset_path("helloworld123466")
+			expect(response.status).to_not eq(200)
+			expect(response.body).to_not include('Reset Password')
+		end
+		
+		it "Update Password Works on valid token and password" do
+			put password_reset_path(@token), params: {user: {password: 'password', password_confirmation: 'password' }}
+			expect(flash[:success]).to eq('Password has been reset!')
+			expect(response).to redirect_to(login_path)
+		end
+		
+		it "Update Password does not Works on mismatch passwords" do
+			put password_reset_path(@token), params: {user: {password: 'password', password_confirmation: '123123' }}
+			expect(response).to redirect_to(edit_password_reset_path)
+		end
+		
+		it "Update Password does not Works on short passwords" do
+			put password_reset_path(@token), params: {user: {password: '123', password_confirmation: '123' }}
+			expect(response).to redirect_to(edit_password_reset_path)
+		end
+  
+  end
+end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,16 +1,54 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    before(:each) do
-      @attr = { :first_name => "Example User",
-                :last_name => "Example User3",
-                :email => "user@example.com",
-                :password => "secret",
-                :password_confirmation => "secret"
-      }
+RSpec.describe "Test Users", type: :request do
+  describe "SignUp, Login and Logout" do
+		
+		before(:each) do
+			@user1 = {email: "jane@doe.com", password: "pw1234", password_confirmation: 'pw1234', first_name: "Anything", last_name: "Lorem ipsum"}
+			@user2 = { user: {email: "john@doe.com", password: "password", password_confirmation: 'password', first_name: "John", last_name: "Doe"}}
+			User.create(@user1)
 		end
 		
+		it "SignUp View Rendered" do
+			get signup_path
+			expect(response.status).to eq(200)
+			expect(response.body).to include("Already have an account?")
+		end
+
+		it "SignUp Works" do
+			post users_path, params: @user2
+			expect(response).to redirect_to(root_path)
+			expect(flash[:success]).to eq("Signup Successful")
+			post login_path, params: { user: {email: @user2[:user][:email], password: @user2[:user][:password]}}
+			expect(response).to redirect_to(root_path)
+			expect(flash[:success]).to eq("You have successfully Logged In")
+		end
+
+		it "Login View Rendered" do
+			get login_path
+			expect(response.status).to eq(200)
+			expect(response.body).to include("Forgot Password?")
+		end
+
+		it "Login Works with right credentials" do
+			post login_path, params: { user: {email: 'jane@doe.com', password: 'pw1234'}}
+			expect(response).to redirect_to(root_path)
+			expect(flash[:success]).to eq("You have successfully Logged In")
+			expect(session[:user_id]).to_not eq(nil)
+		end
+
+		it "Login does not Works with wrong credentials" do
+			post login_path, params: { user: {email: 'jane@doe.com', password: 'pw12345'}}
+			expect(flash[:error]).to eq("Invalid Email or Password")
+			expect(response).to redirect_to(login_path)
+		end
+
+		it "Logout works" do
+			get logout_path
+			expect(response).to redirect_to(login_path)
+			expect(flash[:notice]).to eq("Logged Out")
+			expect(session[:user_id]).to eq(nil)
+		end
   
   end
 end
